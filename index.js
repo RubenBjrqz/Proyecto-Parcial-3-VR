de09512fa5ba42ba40a3689fa219723947f869c0
Rubén Bojórquez <rubenbjrz@gmail.com>
2021-11-25 20:12:12 -0700

    Updates

--- index.js
+++ index.js
@@ -5,23 +5,11 @@ import { VRButton } from './js/examples/jsm/webxr/VRButton.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-init();
-animate();
-
 const renderer = new THREE.WebGLRenderer( { antialias: true } );
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.outputEncoding = THREE.sRGBEncoding;
 renderer.xr.enabled = true;
-container.appendChild( renderer.domElement );
-
-const onWindowResize =()=>{
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-
-  renderer.setSize( window.innerWidth, window.innerHeight );
-}
-
 document.body.appendChild( renderer.domElement );
 
 window.addEventListener('resize', onWindowResize);
@@ -39,6 +27,24 @@ scene.add( light );
 
 const loader = new GLTFLoader();
 
+loader.load( './models/scene.gltf', 
+ ( gltf ) => {
+
+  scene.add( gltf.scene );
+},
+// called while loading is progressing
+( xhr ) => {
+
+  console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+},
+// called when loading has errors
+( error ) => {
+
+  console.log( 'An error happened' );
+
+});
+
 /*camera.position.z = 1200;
 camera.position.y = 500;*/
 
@@ -52,8 +58,10 @@ function onWindowResize() {
 }
 
 function animate() {
-  renderer.setAnimationLoop(()=> {
-    renderer.render( scene, camera)
-  });
+  renderer.setAnimationLoop( render );
+}
+
+function render() {
+  renderer.render(scene, camera);
 }
 animate();
