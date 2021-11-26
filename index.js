e6099dba92e8200b3c764df8e618cde164099f5b
Rubén Bojórquez <rubenbjrz@gmail.com>
2021-11-25 20:06:12 -0700

    Updates

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 import * as THREE from './js/build/three.module.js';
 import {GLTFLoader} from './js/examples/jsm/loaders/GLTFLoader.js'
+import { VRButton } from './js/examples/jsm/webxr/VRButton.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
-renderer.outputEncoding = THREE.sRGBEncoding;
+init();
+animate();
+
+const renderer = new THREE.WebGLRenderer( { antialias: true } );
+renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.xr.enabled = true;
+container.appendChild( renderer.domElement );
+
+const onWindowResize =()=>{
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
 document.body.appendChild( renderer.domElement );
+
+window.addEventListener('resize', onWindowResize);
+
+document.body.appendChild(VRButton.createButton(renderer));
+
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
@@ -19,31 +39,21 @@ scene.add( light );
 
 const loader = new GLTFLoader();
 
-loader.load( './models/scene.gltf', 
- ( gltf ) => {
+/*camera.position.z = 1200;
+camera.position.y = 500;*/
 
-  scene.add( gltf.scene );
-},
-// called while loading is progressing
-( xhr ) => {
+function onWindowResize() {
 
-  console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
 
-},
-// called when loading has errors
-( error ) => {
+  renderer.setSize( window.innerWidth, window.innerHeight );
 
-  console.log( 'An error happened' );
-
-});
-
-camera.position.z = 1200;
-camera.position.y = 500;
+}
 
 function animate() {
-  //cube.rotation.x += 0.01;
-  //cube.rotation.y += 0.01;
-	requestAnimationFrame( animate );
-	renderer.render( scene, camera );
+  renderer.setAnimationLoop(()=> {
+    renderer.render( scene, camera)
+  });
 }
 animate();
